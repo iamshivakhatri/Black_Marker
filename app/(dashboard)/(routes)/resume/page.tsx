cf1368082b6242ea5198dff3691c4876488bd989
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-03-22 23:10:17 -0400

    Small commit

--- app/(dashboard)/(routes)/resume/page.tsx
+++ app/(dashboard)/(routes)/resume/page.tsx
@@ -2,9 +2,39 @@ import { Input } from "@/components/ui/input";
 
 const Resume = () => {
     return (  
-        <div className="flex-col p-5">
-            
+        <div>
+        <h1 className='text-3xl font-bold text-gray-900'>Name</h1>
+        <div className='mt-3 flex items-end justify-between'>
+            <p className='text-2xl text-gray-900'>
+                Hello
+
+            </p>
         </div>
+        <hr  className='my-4'/>
+
+        <div className='flex flex-col gap-y-6'>
+        <div className='flex items-center gap-x-4 '>
+            <h3 className='font-semibold text-black'>Size</h3>
+            Hi
+
+        </div>
+
+        <div className='flex items-center gap-x-4 '>
+            <h3 className='font-semibold text-black'>Color</h3>
+            <div className='h-6 w-6 rounded-full border border-gray-600 ' style={{backgroundColor: "blue"}} />
+           
+        </div>
+        </div>
+
+        <div className='mt-10 flex items-center gap-x-3'>
+            <button>
+                button
+            </button>
+
+        </div>
+        
+    </div>
+
     );
 }
  
