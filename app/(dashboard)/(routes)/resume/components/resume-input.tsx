5d3174b96236eb8e6d75bb5a064c0d7767d29ee5
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-06-12 22:47:08 -0400

    Small ui change

--- app/(dashboard)/(routes)/resume/components/resume-input.tsx
+++ app/(dashboard)/(routes)/resume/components/resume-input.tsx
@@ -11,7 +11,7 @@ const ResumeInput: React.FC<ResumeInputProps> = ({ onChange }) => {
     };
 
     return (
-        <div className="h-screen mx-auto overflow-hidden md:w-2/4 2xl:w-1/4">
+        <div className="h-screen mx-auto overflow-hidden md:w-1/4 ">
             <div className="h-full overflow-y-auto">
                 <ResumeForm />
             </div>
